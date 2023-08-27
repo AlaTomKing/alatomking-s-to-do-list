98a2b506e6bb8974a16bb35506ec9e843fab8f3d
Amogus Bread <89712796+AlaTomKing@users.noreply.github.com>
2023-08-27 20:36:42 +0200

    more mobile updates

--- mainScript.js
+++ mainScript.js
@@ -6,6 +6,25 @@ function processAjaxData(response, urlPath) {
 }
 
 (() => {
+    let db;
+    const request = indexedDB.open("ToDoList");
+
+    request.onerror = (event) => {
+        console.error("Bro, I don't think your IndexedDB work.");
+    };
+
+    request.onsuccess = (event) => {
+        db = event.target.result;
+    };
+
+    request.onupgradeneeded = (event) => {
+        // Save the IDBDatabase interface
+        const db = event.target.result;
+
+        // Create an objectStore for this database
+        const objectStore = db.createObjectStore("name", { keyPath: "myKey" });
+    };
+
     window.onpopstate = function (e) {
         if (e.state) {
             document.getElementById("content").innerHTML = e.state.html;
@@ -13,9 +32,13 @@ function processAjaxData(response, urlPath) {
         }
     };
 
-    document.querySelector("#open_settings").addEventListener("click", () => {
-        window.history.pushState("addSettings", "Title", "?settings");
+    document.querySelector("#github").addEventListener("click", () => {
+        window.open("https://github.com/AlaTomKing/to-do-list")
     })
+
+    // document.querySelector("#open_settings").addEventListener("click", () => {
+    //     window.history.pushState("addSettings", "Title", "?settings");
+    // })
 
     document.querySelector("#new_list").addEventListener("click", () => {
         window.history.pushState("createList", "New List", "?list=hi there");
@@ -25,6 +48,20 @@ function processAjaxData(response, urlPath) {
         get: (searchParams, prop) => searchParams.get(prop),
     });
 
-    console.log(params.settings === "" && "settings exist" || "it dont exis")
+    // console.log(params.settings === "" && "settings exist" || "it dont exis")
     console.log(params.list)
+
+    window.addEventListener("keydown", (e) => {
+        switch (e.code) {
+            case "ArrowLeft":
+                console.log("arrow left")
+                break
+            case "ArrowRight":
+                console.log("arrow right")
+                break
+            case "Space":
+                console.log("space")
+                break
+        }
+    })
 })()
